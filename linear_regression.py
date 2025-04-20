1d11b1f90ee78f0245fa050cbf4b5eb6894dd6a7
Learner354 <forlearning.354@gmail.com>
2025-04-20 17:29:37 +0500

    Create linear_regression.py
    implementing Linear regression from scratch using numpy

---
+++ linear_regression.py
@@ -0,0 +1,9 @@
+class linear_regression:
+  def __init__(self,learning_rate,iterations):
+    self.iterations=iterations
+    self.lr=learning_rate
+    self.weight=[]
+    self.bias=0
+  def fit(self,X,y)
+
+  
